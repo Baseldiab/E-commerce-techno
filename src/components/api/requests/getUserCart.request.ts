08e0e1014e1842913bd5c0f2ca4ad3c4fe3f35d5
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2024-07-26 04:33:13 +0300

    update error on cart page

--- src/components/api/requests/getUserCart.request.ts
+++ src/components/api/requests/getUserCart.request.ts
@@ -1,26 +1,67 @@
-import { useGlobalStore } from "../../../store/global";
-import { errorNotification } from "../../notifications/notifications";
-import { GET_USER_CARTS } from "../constants";
+// import { useGlobalStore } from "../../../store/global";
+// import { errorNotification } from "../../notifications/notifications";
+// import { GET_USER_CARTS } from "../constants";
 
-export async function get_user_carts() {
+// export async function get_user_carts() {
+//   const { setLoading } = useGlobalStore.getState();
+
+//   try {
+//     setLoading(true);
+
+//     const response = await fetch(GET_USER_CARTS(), { cache: "no-cache" });
+
+//     const body = await response.json();
+
+//     return body;
+//   } catch (e) {
+//     console.log(e);
+//     errorNotification("Bad Request");
+
+//     return {
+//       data: {},
+//     };
+//   } finally {
+//     setLoading(false); // Set loading to false after fetching data
+//   }
+// }
+import { useGlobalStore } from '../../../store/global';
+import { errorNotification } from '../../notifications/notifications';
+import { CartModel } from '../../types/cartModel';
+import { getCachedData, setCachedData } from '../cashingUtility';
+import { GET_USER_CARTS } from '../constants';
+
+export interface UserCart {
+  id: string;
+  items: { productId: string; quantity: number }[];
+}
+
+export const get_user_carts = async (): Promise<CartModel[]> => {
+  const cacheKey = 'userCarts';
+  const cachedData = getCachedData<CartModel[]>(cacheKey);
+
+  if (cachedData) {
+    return cachedData;
+  }
+
   const { setLoading } = useGlobalStore.getState();
 
   try {
     setLoading(true);
 
-    const response = await fetch(GET_USER_CARTS(), { cache: "no-cache" });
+    const response = await fetch(GET_USER_CARTS(), { cache: 'no-cache' });
 
-    const body = await response.json();
+    if (!response.ok) {
+      throw new Error('Error fetching user carts');
+    }
 
+    const body: CartModel[] = await response.json();
+    setCachedData(cacheKey, body);
     return body;
   } catch (e) {
     console.log(e);
-    errorNotification("Bad Request");
-
-    return {
-      data: {},
-    };
+    errorNotification('Bad Request');
+    return [];
   } finally {
     setLoading(false); // Set loading to false after fetching data
   }
-}
+};
