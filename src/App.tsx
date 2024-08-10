e4ed5a895f8233b5e48bbb721ea87079a495d385
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2024-08-10 15:31:59 +0300

    fix loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
 import { Outlet } from "react-router-dom";
-import DesktopNavbar from "./components/navbar/desktop.navbar";
-import Footer from "./components/footer/footer";
+import { Suspense, lazy } from "react";
+// import DesktopNavbar from "./components/navbar/desktop.navbar";
+// import Footer from "./components/footer/footer";
 import ScrollToTop from "./components/global/scrollToTop";
 import LoadingLayout from "./components/layout/Loading.layout";
 import { useGlobalStore } from "./store/global";
+
+// Lazy load components
+const LazyDesktopNavbar = lazy(() => import("./components/navbar/desktop.navbar"));
+const LazyTopNavbar = lazy(() => import("./components/navbar/navbarComponents/topNavbar"));
+const LazyFooter = lazy(() => import("./components/footer/footer"));
 
 function App() {
   const { loading } = useGlobalStore();
 
   return (
     <>
-      {loading ? (
-        <LoadingLayout />
-      ) : (
-        <div className="min-h-screen m-0 flex flex-col">
-          <main className="flex-1">
-            <ScrollToTop />
-            <DesktopNavbar />
-            <Outlet />
-          </main>
-          <Footer />
-        </div>
-      )}
+      <div className="min-h-screen m-0 flex flex-col">
+        <main className="flex-1">
+          <ScrollToTop />
+
+          <Suspense fallback={<LoadingLayout />}>
+            <LazyTopNavbar />
+            <LazyDesktopNavbar />
+          </Suspense>
+
+          <Suspense fallback={<LoadingLayout />}>
+            {loading ? (
+              <LoadingLayout />
+            ) : (
+              <>
+                <Outlet />
+              </>
+            )}
+          </Suspense>
+        </main>
+
+        <Suspense fallback={<LoadingLayout />}>
+          <LazyFooter />
+        </Suspense>
+      </div>
     </>
   );
 }
--- src/components/navbar/desktop.navbar.tsx
+++ src/components/navbar/desktop.navbar.tsx
@@ -1,11 +1,9 @@
 import MainNavbar from "./navbarComponents/mainNavbar";
 import MobileNavbar from "./navbarComponents/mobileNavbar";
-import TopNavbar from "./navbarComponents/topNavbar";
 
 export default function DesktopNavbar() {
   return (
     <nav>
-      <TopNavbar />
       <MainNavbar />
       <MobileNavbar />
     </nav>
